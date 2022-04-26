39f0383a9afdf216598a5f4434e7edea43301319
Akash187 <kumarakash007@gmail.com>
2022-04-27 01:25:09 +0530

    refactor: modified ProtectedRoute and NotProtectedRoute to accept ReactJSXElement

--- src/routes/NotProtectedRoute.tsx
+++ src/routes/NotProtectedRoute.tsx
@@ -1,13 +1,10 @@
 import { Navigate } from 'react-router-dom'
 import { useAppStore } from '../store/appStore'
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace'
 
-const NotProtectedRoute = ({
-	Component
-}: {
-	Component: React.FunctionComponent
-}) => {
+const NotProtectedRoute = ({ Component }: { Component: ReactJSXElement }) => {
 	const { isLoggedIn } = useAppStore()
 
-	return isLoggedIn ? <Navigate to="/" replace /> : <Component />
+	return isLoggedIn ? <Navigate to="/" replace /> : Component
 }
 export default NotProtectedRoute
--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,13 +1,10 @@
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace'
 import { Navigate } from 'react-router-dom'
 import { useAppStore } from '../store/appStore'
 
-const ProtectedRoute = ({
-	Component
-}: {
-	Component: React.FunctionComponent
-}) => {
+const ProtectedRoute = ({ Component }: { Component: ReactJSXElement }) => {
 	const { isLoggedIn } = useAppStore()
 
-	return isLoggedIn ? <Component /> : <Navigate to="/" replace />
+	return isLoggedIn ? Component : <Navigate to="/" replace />
 }
 export default ProtectedRoute
