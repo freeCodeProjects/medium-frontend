04221a8d2f4545c74773f91d4da08279d9729830
Akash187 <kumarakash007@gmail.com>
2022-07-21 04:01:20 +0530

    refactor: minor code refactoring

--- src/components/blog/BlogPreview.tsx
+++ src/components/blog/BlogPreview.tsx
@@ -4,7 +4,7 @@ import BoldTypography from '../ui/BoldTypography'
 import dayjs from 'dayjs'
 import SmallChip from '../ui/SmallChip'
 import Bookmark from '../ui/Bookmark'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 type IProps = {
 	blogPreview: BlogWithUserData
@@ -13,94 +13,94 @@ type IProps = {
 const BlogPreview = ({ blogPreview }: IProps) => {
 	const navigate = useNavigate()
 
+	const navigateToDetailPage = () => {
+		navigate(`/blog/${blogPreview.slug}`)
+	}
+
 	return (
-		<Link
-			to={`/blog/${blogPreview.slug}`}
-			className="link"
-			style={{ height: '100%', display: 'flex' }}>
+		<Box
+			onClick={navigateToDetailPage}
+			sx={{
+				flex: 1,
+				bgcolor: 'secondary.main',
+				padding: '1rem',
+				display: 'flex',
+				justifyContent: 'space-between',
+				gap: '0.5rem',
+				cursor: 'pointer'
+			}}>
 			<Box
 				sx={{
-					flex: 1,
-					bgcolor: 'secondary.main',
-					padding: '1rem',
-					display: 'flex',
-					justifyContent: 'space-between',
-					gap: '0.5rem',
-					cursor: 'pointer'
+					display: 'grid',
+					gridTemplateRows: 'auto auto auto 1fr',
+					gap: '0.35rem',
+					overflow: 'hidden',
+					width: '100%'
 				}}>
 				<Box
 					sx={{
+						display: 'flex',
+						alignItems: 'center',
+						gap: '0.5rem'
+					}}>
+					<Avatar
+						sx={{ width: 24, height: 24 }}
+						alt={blogPreview.user.name}
+						src={blogPreview.user.photo}
+					/>
+					<BoldTypography className="truncate-2" variant="subtitle2">
+						{blogPreview.user.name}
+					</BoldTypography>
+				</Box>
+				<BoldTypography className="truncate-2">
+					{blogPreview.publishedTitle}
+				</BoldTypography>
+
+				<Typography className="truncate-2" variant="subtitle2">
+					{blogPreview.subTitle}
+				</Typography>
+				<Box
+					sx={{
+						alignSelf: 'end',
+						alignItems: 'center',
 						display: 'grid',
-						gridTemplateRows: 'auto auto auto 1fr',
-						gap: '0.35rem',
-						overflow: 'hidden',
+						gridTemplateColumns: 'auto auto auto 1fr',
+						gap: '0.5rem',
+						overflowX: 'hidden',
 						width: '100%'
 					}}>
-					<Box
-						sx={{
-							display: 'flex',
-							alignItems: 'center',
-							gap: '0.5rem'
-						}}>
-						<Avatar
-							sx={{ width: 24, height: 24 }}
-							alt={blogPreview.user.name}
-							src={blogPreview.user.photo}
-						/>
-						<BoldTypography className="truncate-2" variant="subtitle2">
-							{blogPreview.user.name}
-						</BoldTypography>
-					</Box>
-					<BoldTypography className="truncate-2">
-						{blogPreview.publishedTitle}
-					</BoldTypography>
-
-					<Typography className="truncate-2" variant="subtitle2">
-						{blogPreview.subTitle}
+					<Typography variant="subtitle2" sx={{ whiteSpace: 'nowrap' }}>
+						{dayjs(blogPreview.publishedAt).format('MMM D')}
 					</Typography>
-					<Box
-						sx={{
-							alignSelf: 'end',
-							alignItems: 'center',
-							display: 'grid',
-							gridTemplateColumns: 'auto auto auto 1fr',
-							gap: '0.5rem',
-							overflowX: 'hidden',
-							width: '100%'
-						}}>
-						<Typography variant="subtitle2" sx={{ whiteSpace: 'nowrap' }}>
-							{dayjs(blogPreview.publishedAt).format('MMM D')}
-						</Typography>
-						<Typography variant="subtitle2" sx={{ whiteSpace: 'nowrap' }}>
-							{blogPreview.readTime} read
-						</Typography>
-						{blogPreview.tags.length > 0 ? (
-							<SmallChip label={blogPreview.tags[0]} />
-						) : (
-							<div></div>
-						)}
-						<Box sx={{ justifySelf: 'end' }}>
-							<Bookmark />
-						</Box>
+					<Typography variant="subtitle2" sx={{ whiteSpace: 'nowrap' }}>
+						{blogPreview.readTime} read
+					</Typography>
+					{blogPreview.tags.length > 0 ? (
+						<SmallChip label={blogPreview.tags[0]} />
+					) : (
+						<div></div>
+					)}
+					<Box sx={{ justifySelf: 'end' }}>
+						<Bookmark blogId={blogPreview._id} />
 					</Box>
 				</Box>
-				{blogPreview.previewImage && (
-					<Box
-						component="img"
-						sx={{
-							minWidth: 156,
-							maxWidth: 156,
-							height: 156,
-							objectFit: 'cover',
-							display: { xs: 'none', sm: 'block' }
-						}}
-						src={blogPreview.previewImage}
-						alt={blogPreview.publishedTitle}
-						loading="lazy"
-					/>
-				)}
 			</Box>
-		</Link>
+			{blogPreview.previewImage && (
+				<Box
+					component="img"
+					sx={{
+						minWidth: 156,
+						maxWidth: 156,
+						height: 156,
+						objectFit: 'cover',
+						display: { xs: 'none', sm: 'block' }
+					}}
+					src={blogPreview.previewImage}
+					alt={blogPreview.publishedTitle}
+					loading="lazy"
+				/>
+			)}
+		</Box>
 	)
 }
 export default BlogPreview
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -40,127 +40,129 @@ const Blog = () => {
 			) : isError ? (
 				<NotFound message="Blog Not found." />
 			) : (
-				<Box
-					sx={{
-						display: 'flex',
-						paddingY: '1rem',
-						flexDirection: { xs: 'column-reverse', md: 'unset' }
-					}}>
+				blog && (
 					<Box
 						sx={{
 							display: 'flex',
-							flex: 1,
-							alignItems: 'center',
-							justifyContent: 'center',
-							position: { md: 'fixed' },
-							top: 64,
-							bottom: 64,
-							maxWidth: { xs: '650px', md: '240px' },
-							alignSelf: 'center',
-							flexDirection: { xs: 'column-reverse', md: 'column' }
+							paddingY: '1rem',
+							flexDirection: { xs: 'column-reverse', md: 'unset' }
 						}}>
 						<Box
 							sx={{
-								padding: '1rem',
 								display: 'flex',
-								gap: { xs: '1rem', md: '0.5rem' },
-								flexDirection: { md: 'column' },
-								alignItems: 'center'
+								flex: 1,
+								alignItems: 'center',
+								justifyContent: 'center',
+								position: { md: 'fixed' },
+								top: 64,
+								bottom: 64,
+								maxWidth: { xs: '650px', md: '240px' },
+								alignSelf: 'center',
+								flexDirection: { xs: 'column-reverse', md: 'column' }
 							}}>
-							<Avatar
-								alt={blog?.user.name}
-								src={blog?.user.photo}
+							<Box
 								sx={{
-									width: { xs: 64, sm: 120 },
-									height: { xs: 64, sm: 120 },
-									alignSelf: { xs: 'start', md: 'center' }
-								}}
-							/>
+									padding: '1rem',
+									display: 'flex',
+									gap: { xs: '1rem', md: '0.5rem' },
+									flexDirection: { md: 'column' },
+									alignItems: 'center'
+								}}>
+								<Avatar
+									alt={blog?.user.name}
+									src={blog?.user.photo}
+									sx={{
+										width: { xs: 64, sm: 120 },
+										height: { xs: 64, sm: 120 },
+										alignSelf: { xs: 'start', md: 'center' }
+									}}
+								/>
+								<Box
+									sx={{
+										display: 'flex',
+										gap: '0.5rem',
+										flexDirection: 'column',
+										alignItems: { md: 'center' }
+									}}>
+									<BoldTypography>{blog?.user.name!}</BoldTypography>
+									<Typography
+										variant="subtitle2"
+										sx={{ textAlign: { md: 'center' } }}>
+										{blog?.user.bio}
+									</Typography>
+									<Box
+										sx={{
+											display: 'flex',
+											justifyContent: { xs: 'space-between' },
+											flexDirection: { md: 'column' },
+											flexWrap: 'wrap',
+											gap: '0.5rem',
+											alignItems: 'center'
+										}}>
+										<BoldTypography>{`${blog?.user.followerCount} followers`}</BoldTypography>
+										<FollowButton />
+									</Box>
+								</Box>
+							</Box>
 							<Box
 								sx={{
 									display: 'flex',
 									gap: '0.5rem',
-									flexDirection: 'column',
-									alignItems: { md: 'center' }
-								}}>
-								<BoldTypography>{blog?.user.name!}</BoldTypography>
-								<Typography
-									variant="subtitle2"
-									sx={{ textAlign: { md: 'center' } }}>
-									{blog?.user.bio}
-								</Typography>
-								<Box
-									sx={{
-										display: 'flex',
-										justifyContent: { xs: 'space-between' },
-										flexDirection: { md: 'column' },
-										flexWrap: 'wrap',
-										gap: '0.5rem',
-										alignItems: 'center'
-									}}>
-									<BoldTypography>{`${blog?.user.followerCount} followers`}</BoldTypography>
-									<FollowButton />
-								</Box>
-							</Box>
-						</Box>
-						<Box
-							sx={{
-								display: 'flex',
-								gap: '0.5rem',
-								width: '100%',
-								justifyContent: { xs: 'space-between', md: 'space-evenly' }
-							}}>
-							<Box sx={{ display: 'flex', alignItems: 'center' }}>
-								<ClapButton />
-								{blog?.clapsCount}
-							</Box>
-							<Box sx={{ display: 'flex', alignItems: 'center' }}>
-								<CommentButton />
-								{blog?.responsesCount}
-							</Box>
-							<Bookmark />
-						</Box>
-					</Box>
-					<Box
-						sx={{
-							display: 'flex',
-							justifyContent: 'center',
-							flex: 1,
-							marginLeft: { md: '240px' }
-						}}>
-						<Box
-							sx={{
-								maxWidth: '650px',
-								display: 'flex',
-								gap: '0.5rem',
-								flexDirection: 'column'
-							}}>
-							<Box sx={{ display: 'flex', gap: '1rem' }}>
-								<Typography variant="subtitle2" sx={{ whiteSpace: 'nowrap' }}>
-									{dayjs(blog?.publishedAt).format('DD MMM, YYYY')}
-								</Typography>
-								<Typography variant="subtitle2" sx={{ whiteSpace: 'nowrap' }}>
-									{blog?.readTime} read
-								</Typography>
-							</Box>
-							<Typography variant="h4">{blog?.publishedTitle}</Typography>
-							<Editor data={blog?.publishedContent} readOnly={true} />
-							<Box sx={{ display: 'flex', gap: '1rem', flexWrap: 'wrap' }}>
-								{blog?.tags.map((tag, index) => (
-									<Chip label={tag} key={index} />
-								))}
-							</Box>
-							<Box
-								component="hr"
-								sx={{
 									width: '100%',
-									display: { xs: 'block', md: 'none' },
-									opacity: '30%'
-								}}
-							/>
+									justifyContent: { xs: 'space-between', md: 'space-evenly' }
+								}}>
+								<Box sx={{ display: 'flex', alignItems: 'center' }}>
+									<ClapButton />
+									{blog?.clapsCount}
+								</Box>
+								<Box sx={{ display: 'flex', alignItems: 'center' }}>
+									<CommentButton />
+									{blog?.responsesCount}
+								</Box>
+								<Bookmark blogId={blog._id!} />
+							</Box>
+						</Box>
+						<Box
+							sx={{
+								display: 'flex',
+								justifyContent: 'center',
+								flex: 1,
+								marginLeft: { md: '240px' }
+							}}>
+							<Box
+								sx={{
+									maxWidth: '650px',
+									display: 'flex',
+									gap: '0.5rem',
+									flexDirection: 'column'
+								}}>
+								<Box sx={{ display: 'flex', gap: '1rem' }}>
+									<Typography variant="subtitle2" sx={{ whiteSpace: 'nowrap' }}>
+										{dayjs(blog?.publishedAt).format('DD MMM, YYYY')}
+									</Typography>
+									<Typography variant="subtitle2" sx={{ whiteSpace: 'nowrap' }}>
+										{blog?.readTime} read
+									</Typography>
+								</Box>
+								<Typography variant="h4">{blog?.publishedTitle}</Typography>
+								<Editor data={blog?.publishedContent} readOnly={true} />
+								<Box sx={{ display: 'flex', gap: '1rem', flexWrap: 'wrap' }}>
+									{blog?.tags.map((tag, index) => (
+										<Chip label={tag} key={index} />
+									))}
+								</Box>
+								<Box
+									component="hr"
+									sx={{
+										width: '100%',
+										display: { xs: 'block', md: 'none' },
+										opacity: '30%'
+									}}
+								/>
+							</Box>
 						</Box>
 					</Box>
-				</Box>
+				)
 			)}
 		</Box>
 	)
